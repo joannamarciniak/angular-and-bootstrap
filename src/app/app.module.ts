aff078f84ecd43455387183bc0fd4078252dd631
joannamarciniak <marciniak.joanna.mj@gmail.com>
2020-01-13 21:56:32 +0100

    Adding components and bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { OfferComponent } from './offer/offer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReferencesComponent } from './references/references.component';
+import { ReferenceComponent } from './reference/reference.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    WorkComponent,
+    OfferComponent,
+    ContactComponent,
+    ReferencesComponent,
+    ReferenceComponent
   ],
   imports: [
     BrowserModule,
